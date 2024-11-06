f703ee08f76688eed0c2bb720f13390e43c9b456
mega025 <xinjiepan123@gmail.com>
2024-11-06 11:30:27 +0100

    casas

--- src/app/informacion/informacion.component.ts
+++ src/app/informacion/informacion.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import{Pokemon} from"../services/interfaces/pokemon";
 @Component({
   selector: 'app-informacion',
   templateUrl: './informacion.component.html',
   styleUrl: './informacion.component.scss'
 })
 export class InformacionComponent {
+
+  pokemon: Pokemon[] = [
+    {id: 1, nombre:"", descripcion: "", imagen_url: ""}
+  ]
 
 }
