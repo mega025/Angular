30691332ce1cb374b4baf73ffc418a60fa3bd0c2
mega025 <xinjiepan123@gmail.com>
2024-11-06 14:23:05 +0100

    pokemon

--- src/app/informacion/informacion.component.ts
+++ src/app/informacion/informacion.component.ts
@@ -8,7 +8,17 @@ import{Pokemon} from"../services/interfaces/pokemon";
 export class InformacionComponent {
 
   pokemon: Pokemon[] = [
-    {id: 1, nombre:"", descripcion: "", imagen_url: ""}
+    {id: 1, nombre:"Pachirisu", descripcion: "Forma parte del grupo de Pokémon " +
+        "que posee bolsas de electricidad en las mejillas." +
+        " Descarga por la cola la electricidad que acumula.",
+      imagen_url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/417.png"},
+    {id: 2, nombre:"Arcanine ", descripcion: "Cuenta un antiguo pergamino que la gente" +
+        " se quedaba fascinada al verlo correr por las praderas.\n" +
+        "\n", imagen_url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/059.png"},
+    {id: 3, nombre:"Rapidash ", descripcion: "Su crin de fuego centellea cuando galopa como una flecha a velocidades " +
+        "que llegan a alcanzar los 240 km/h.", imagen_url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/078.png"},
+    {id: 4, nombre:"Gengar", descripcion: "Para quitarle la vida a su presa, " +
+        "se desliza en su sombra y espera su oportunidad en silencio.", imagen_url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/094.png"}
   ]
 
 }
