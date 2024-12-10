4d3cae99bac1e3d49e22d9bb6077792c4f29e779
mega025 <xinjiepan123@gmail.com>
2024-12-10 14:25:11 +0100

    crear usuario desde el front para guardarlo en una base de datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { PanelControlComponent } from './panel-control/panel-control.component';
 import { LayoutConHeaderComponent } from './layout-con-header/layout-con-header.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { RegistroComponent } from './registro/registro.component';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +25,10 @@ import { RegistroComponent } from './registro/registro.component';
     ReactiveFormsModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(
+  withInterceptorsFromDi()    )
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
