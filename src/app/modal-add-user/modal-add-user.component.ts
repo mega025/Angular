fd7860395355d603617986daf86744ed1d20e5ee
mega025 <xinjiepan123@gmail.com>
2024-12-16 14:01:53 +0100

    avance un poco el formulario

--- src/app/modal-add-user/modal-add-user.component.ts
+++ src/app/modal-add-user/modal-add-user.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import *  as bootstrap from 'bootstrap';
 import {Modal} from 'bootstrap';
+import {NewUser} from '../services/interfaces/usuario';
+import {Router} from '@angular/router';
+import {RegisterService} from '../services/auth/register.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-modal-add-user',
@@ -8,6 +12,23 @@ import {Modal} from 'bootstrap';
   styleUrl: './modal-add-user.component.scss'
 })
 export class ModalAddUserComponent {
+  registerForm: FormGroup;
+
+
+  constructor(
+    private router: Router,
+    private registroService: RegisterService,
+    public formBuilder: FormBuilder,
+
+  ) { this.registerForm = formBuilder.group({
+    username: ['', [Validators.required]],
+    nombre: ['', [Validators.required]],
+    edad: ['0'],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]],
+    confirmPassword: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]],
+  })
+  }
 
   closeModal(){
 const modalElement : HTMLElement | null = document.getElementById("myModal");
@@ -16,5 +37,6 @@ const modalIntance : Modal| null = bootstrap.Modal.getInstance(modalElement);
 modalIntance?.hide();
 }
   }
+
 
 }
