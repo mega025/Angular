066d3ca0857952d7a2294ce4fd95c77d62e48cf2
mega025 <xinjiepan123@gmail.com>
2024-12-17 11:54:10 +0100

    marvel

---
+++ src/app/services/utils/marvelAuth.ts
@@ -0,0 +1,15 @@
+import * as CryptoJS from 'crypto-js';
+
+export  function  MarvelAuthParams(){
+  const ts = new Date().getTime().toString();
+  const  pubKey = "936630ef2c17caffd88799810f2b55db";
+  const privKey = "06d173b6cf3a969c8707a4f425e35b9422f21cbf";
+
+  const has = CryptoJS.MD5(ts + privKey + pubKey).toString();
+
+  return{
+    ts,
+    apikey: pubKey,
+    has
+  }
+}
